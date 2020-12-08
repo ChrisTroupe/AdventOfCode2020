3aa0a49115c76e82eae96fd4958eb3ee3b150d73
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2020-12-07 20:02:17 -0600

    refactor

--- tm_solutions/lib/day7.ex
+++ tm_solutions/lib/day7.ex
@@ -1,27 +1,36 @@
 defmodule Day7 do
 
-  @quantity_map Map.new
-
   def part1() do
     bag_graph = :digraph.new
 
-    load_data()
-    |> Enum.each(&(add_line_to_vars(bag_graph, &1)))
+    load_data(bag_graph)
 
     contain_set = get_containing_bags(bag_graph, ["shiny gold"], MapSet.new)
     MapSet.size contain_set
   end
 
-  def load_data() do
+  def part2() do
+    bag_graph = :digraph.new
+
+    quant_map = load_data(bag_graph)
+
+    IO.inspect(quant_map)
+
+    get_all_bags_inside(bag_graph, quant_map, ["shiny gold"], 0)
+  end
+
+  defp load_data(graph) do
     File.stream!("inputs/day7.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(&(String.trim_trailing(&1, ".")))
+    |> Stream.map(&(add_line_to_vars(graph, &1)))
+    |> Enum.reduce(&(Map.merge(&1, &2)))
   end
 
 
   # Part 1
   # Adds nodes to the digraph and adds to the quantity insides for a node
-  def add_line_to_vars(graph, line) do
+  defp add_line_to_vars(graph, line) do
     [outside, insides] = line
     |> String.split("bags contain", trim: true)
     |> Enum.map(&String.trim/1)
@@ -33,7 +42,7 @@ defmodule Day7 do
     |> Stream.map(&String.trim/1)
     |> Enum.map(&get_bag_info/1)
 
-    Map.put(@quantity_map, outside, insides)
+    inside_map = Enum.reduce(insides, Map.new, fn {quantity, bag_name}, acc -> Map.put(acc, bag_name, quantity) end)
 
     insides = Enum.map(insides, fn {_, bag_name} -> bag_name end)
 
@@ -44,10 +53,18 @@ defmodule Day7 do
       :digraph.add_edge(graph, outside, bag_name)
     end)
 
-    nil
+    %{outside => inside_map}
   end
 
-  def add_vertex(graph, v) do
+  defp get_bag_info(quantity_bag_desc) do
+    matches = Regex.run(~r"^(\d)+\ (.*)$", quantity_bag_desc)
+    case matches do
+      [_, num, type] -> {String.to_integer(num), type}
+      _ -> {:nil, :nil}
+    end
+  end
+
+  defp add_vertex(graph, v) do
     if :digraph.vertex(graph, v) == false do
       :digraph.add_vertex(graph, v)
     end
@@ -55,19 +72,37 @@ defmodule Day7 do
     nil
   end
 
-  def get_containing_bags(_graph, [], acc_set), do: acc_set
-  def get_containing_bags(graph, [bag | process_bags], acc_set) do
+  defp get_containing_bags(_graph, [], acc_set), do: acc_set
+  defp get_containing_bags(graph, [bag | process_bags], acc_set) do
     containing_bags = :digraph.in_neighbours(graph, bag)
     containing_set = MapSet.new(containing_bags)
     get_containing_bags(graph, containing_bags ++ process_bags, MapSet.union(acc_set, containing_set))
   end
 
-  def get_bag_info(quantity_bag_desc) do
-    matches = Regex.run(~r"^(\d)+\ (.*)$", quantity_bag_desc)
-    case matches do
-      [_, num, type] -> {String.to_integer(num), type}
-      _ -> {:nil, :nil}
-    end
+  # Part 2
+  defp get_all_bags_inside(_graph, _quant_map, [], acc), do: acc
+  defp get_all_bags_inside(graph, quant_map, [bag | process_bags], acc) do
+    bags_inside = :digraph.out_neighbours(graph, bag)
+
+
+    total_inside = bags_inside
+    # |> inspect_stream()
+    |> Stream.map(fn in_bag -> quant_map[bag][in_bag] end)
+    |> Stream.filter(fn
+      nil -> false
+      _ -> true
+    end)
+    |> Enum.sum
+
+    get_all_bags_inside(graph, quant_map, bags_inside ++ process_bags, acc + total_inside)
+  end
+
+  def inspect_stream(stream) do
+    stream
+    |> Enum.to_list()
+    |> IO.inspect()
+
+    stream
   end
 
 end
